3dcb421525005cc37f5014fa96dfa95f85765278
Kristina Bekher <krbekher@gmail.com>
2024-11-13 11:21:13 +0100

    Add storybook-static

---
+++ storybook-static/main.3cf9ee747e1e51e8279f.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(488),c(635),n.exports=c(963)},1:function(n,o){},635:function(n,o,c){"use strict";c.r(o);c(636),c(863),c(866)}},[[0,1,2]]]);
